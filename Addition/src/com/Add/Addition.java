83d3badcb1d88530068eea1eaa6aa029cef9fc33
amits <amits@ITPL-DSK-022.itarium.local>
2022-08-11 13:25:19 +0530

    done

---
+++ Addition/src/com/Add/Addition.java
@@ -0,0 +1,17 @@
+package com.Add;
+
+public class Addition
+{
+	public static void main(String[] args)
+	{
+		int Ans;
+		
+		int A=2;
+		int B=5;
+		
+		Ans = A+B;
+		
+		
+		System.out.println("This is the addition: "+ Ans);
+	}
+}
